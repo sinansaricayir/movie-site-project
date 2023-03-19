d37eef02a380d7986019a724ac417e11356647ef
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-03-19 23:24:47 +0300

    responsive

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 const arrows = document.querySelectorAll(".arrow")
 const movieLists = document.querySelectorAll(".movie-list")
 
-arrows.forEach((arrow, i) => {
 
+arrows.forEach((arrow, i) => {
+    const widthRatio = Math.floor(window.innerWidth / 300)
     let clickCounter = 0
     const imageItem = movieLists[i].querySelectorAll("img").length
 
@@ -12,10 +13,11 @@ arrows.forEach((arrow, i) => {
 
         clickCounter++
 
-        if (imageItem - (4 + clickCounter) >= 0) {
+        if (imageItem - (4 + clickCounter) + (4 - widthRatio) >= 0) {
             movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`
         } else {
             movieLists[i].style.transform = "translateX(0)"
+            clickCounter = 0
         }
 
     })
@@ -31,3 +33,5 @@ const items = document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,
 ball.addEventListener("click", function () {
     items.forEach((item) => item.classList.toggle("active"))
 })
+
+
